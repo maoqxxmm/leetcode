5f41a088b720611c5814007b796503a09d100408
maoqxm <maoqxm@gmail.com>
2022-04-08 00:31:37 +0800

    chore: daily

---
+++ problems/169.多数元素.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=javascript
+ *
+ * [169] 多数元素
+ *
+ * https://leetcode-cn.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (66.62%)
+ * Likes:    1390
+ * Dislikes: 0
+ * Total Accepted:    495.3K
+ * Total Submissions: 743.3K
+ * Testcase Example:  '[3,2,3]'
+ *
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：[3,2,3]
+ * 输出：3
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：[2,2,1,1,1,2,2]
+ * 输出：2
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ *
+ * 尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  let candidate = nums[0];
+  let count = 0;
+  nums.forEach((num) => {
+    if (num === candidate) {
+      count++;
+    } else if (count === 0) {
+      candidate = num;
+      count++;
+    } else {
+      count--;
+    }
+  });
+  return candidate;
+};
+// @lc code=end
---
+++ problems/34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=javascript
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (42.20%)
+ * Likes:    1589
+ * Dislikes: 0
+ * Total Accepted:    492.4K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ *
+ * 进阶：
+ *
+ *
+ * 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0
+ * -10^9
+ * nums 是一个非递减数组
+ * -10^9
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  const binarySearch = (lower) => {
+    let left = 0;
+    let right = nums.length - 1;
+    let ans = nums.length;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (nums[mid] > target || (lower && nums[mid] >= target)) {
+        right = mid - 1;
+        ans = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return ans;
+  };
+  const leftIndex = binarySearch(true);
+  const rightIndex = binarySearch() - 1;
+  if (
+    leftIndex <= rightIndex &&
+    rightIndex <= nums.length - 1 &&
+    nums[leftIndex] === target &&
+    nums[rightIndex] === target
+  ) {
+    return [leftIndex, rightIndex];
+  } else {
+    return [-1, -1];
+  }
+};
+// @lc code=end
