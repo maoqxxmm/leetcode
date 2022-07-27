b0288a68c0ba0ef2610de612b9118290775cbc93
maoqxxmm <maoqxm@gmail.com>
2022-07-27 23:50:10 +0800

    chore: daily

---
+++ problems/448.找到所有数组中消失的数字.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    const index = Math.abs(nums[i]) - 1;
+    nums[index] = -Math.abs(nums[index]);
+  }
+  const res = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      res.push(i + 1);
+    }
+  }
+  return res;
+};
